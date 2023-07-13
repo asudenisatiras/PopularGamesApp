306b70bba7027f30c71033db36dd7b3d0a28155c
Asude Nisa Tıraş <asudenisatiras@gmail.com>
2023-07-14 00:17:16 +0300

    First Commit
    The main storyboard has been deleted, info.the plist has been updated. The package for modularity, workspace was created.

---
+++ PopularGamesAPP/PopularGamesAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopularGamesAPP
+//
+//  Created by Asude Nisa Tıraş on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
